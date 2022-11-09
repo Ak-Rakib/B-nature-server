c41aa799bfe55a9696ab898dbb6161899182bbc7
unknown <ak-rakib>
2022-11-09 01:02:41 -0800

    get single data for service page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+const { ObjectID } = require('bson');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -26,6 +27,13 @@ async function run () {
         const cursor = reviewCollection.find(query);
         const result = await cursor.toArray();
         res.send(result);
+        })
+
+        app.get('/review/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id:ObjectID(id) }
+            const result = await reviewCollection.findOne(query);
+            res.send(result);
         })
 
         app.get('/projects', async (req, res) => {
