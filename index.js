3835002afbff91ead1b82535d4ec36e5458754bb
unknown <ak-rakib>
2022-11-10 02:54:38 -0800

    review data read

--- index.js
+++ index.js
@@ -13,35 +13,53 @@ app.use(express.json())
 
 // --------------------------Mongodb Connect-----------------
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fvciqgr.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 // -------------------------CRUD operation-------------------
 async function run () {
     try{
-        const reviewCollection = client.db('Bnature').collection('reviews');
-        const projectCollection = client.db('Bnature').collection('project');
+        const serviceCollection = client.db('Bnature').collection('reviews');
+        const projectCollection = client.db('Bnature').collection('porject');
+        const reviewCollection = client.db('Bnature').collection('comment')
 
+
+        // ---------------- create ----------------------------
+
+        app.post('/comment',async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review)
+            res.send(result)
+        })
+        
+        // --------------- Read -------------------------------
         app.get('/review', async (req, res) => {
         const query = {};
-        const cursor = reviewCollection.find(query);
+        const cursor = serviceCollection.find(query);
         const result = await cursor.toArray();
         res.send(result);
-        })
-
-        app.get('/review/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id:ObjectID(id) }
-            const result = await reviewCollection.findOne(query);
-            res.send(result);
         })
 
         app.get('/projects', async (req, res) => {
             const query = {};
             const cursor = projectCollection.find(query);
             const result = await cursor.toArray();
-            console.log(result)
             res.send(result);
+        })
+
+        app.get('/review/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id:ObjectID(id) }
+            const result = await serviceCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.get('/comment', async (req, res) => {
+            const query = {};
+            const curson = reviewCollection.find(query);
+            const result = await curson.toArray();
+            res.send(result)
         })
     }
     finally{
